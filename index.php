21294c6ef1d3a35ad847e25cc233c4773445b54d
Alex <64538834+alex-safonov@users.noreply.github.com>
2022-06-28 12:27:08 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+Подключение
+<link rel="stylesheet" type="text/css" href="/static/build/js/phone-check.js?<? echo time(); ?>">
+Отобразится как:
+<link rel="stylesheet" type="text/css" href="/static/build/js/phone-check.js?1656408103">
+
+
+Сборка в Битрикс:
+Page\Asset::getInstance()->addJs('/static/build/js/phone-check.js');
