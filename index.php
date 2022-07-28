fc23434222656e061b7eca464e8822511a933d19
Alex <64538834+alex-safonov@users.noreply.github.com>
2022-07-28 19:54:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <script type="text/javascript" src="/static/build/js/phone-check.js?<? echo time(); ?>"></script>
 Отобразится как:
 <script type="text/javascript" src="/static/build/js/phone-check.js?1656410046"></script>
-
+При дальнейших загрузках версия будет меняться:
+<script type="text/javascript" src="/static/build/js/phone-check.js?1659027002"></script>
 
 Сборка в Битрикс:
 Page\Asset::getInstance()->addJs('/static/build/js/phone-check.js');
